47eb623f0423247cf3b2123b34718f29d6153315
olovm <olov.mckie@ub.uu.se>
2020-09-14 09:22:05 +0000

    working on a new docker setup for running tests

---
+++ docker/entrypoint.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+echo "running entrypoint.sh..."
+pwd
+
+#ls -la
+#ls -la reports/
+#ls -la reports/coverage
+whoami
+#echo "ls -la /home/olov/"
+#ls -la /home/olov/
+#echo "ls -la /home/olov/workspace/cora-jsclient"
+#ls -la /home/olov/workspace/cora-jsclient
+#echo "ls -la /home/olov/trams/"
+#ls -la /home/olov/trams/
+#echo ""
+#mkdir /home/olov/trams/tramsDir
+
+echo "ls -la /home"
+ls -la /home
+echo "ls -la /home/pere"
+ls -la /home/pere
+echo "ls -la /home/pere/trams"
+ls -la /home/pere/trams

38dcac75dadcaed2a113ac94cbc39fad159f434c
olovm <olov.mckie@ub.uu.se>
2020-09-15 13:15:41 +0000

    cleaning up changes

--- dockerForTests/entrypoint.sh
+++ dockerForTests/entrypoint.sh
@@ -1,39 +1,7 @@
 #! /bin/bash
 echo "running entrypoint.sh..."
-pwd
-
-#ls -la
-#ls -la reports/
-#ls -la reports/coverage
-whoami
-#echo "ls -la /home/olov/"
-#ls -la /home/olov/
-#echo "ls -la /home/olov/workspace/cora-jsclient"
-#ls -la /home/olov/workspace/cora-jsclient
-#echo "ls -la /home/olov/trams/"
-#ls -la /home/olov/trams/
-#echo ""
-#mkdir /home/olov/trams/tramsDir
-
-
-echo "ls -la "
-ls -la 
-echo "ls -la src"
-ls -la src
-ls -la 
-
-
 /usr/local/bin/karma start karma.conf.js
-ls -la 
-ls -la reports 
-echo "reports/coverage/firefox"
-ls -la reports/coverage/firefox
-echo "reports/tests"
-ls -la reports/tests
 
 mkdir jsReportFromDocker
 cp reports/tests jsReportFromDocker/test -r
 cp reports/coverage jsReportFromDocker/coverage -r
-
-
-#wait 5000

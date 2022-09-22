fe74296ae4009c6cf5e141ed1ad4789d8217d407
perebartrolisimo <pere.bartroli@ub.uu.se>
2022-09-22 08:32:18 +0000

    remove QUnit only

--- src/test/script/presentation/pCollectionVarTest.js
+++ src/test/script/presentation/pCollectionVarTest.js
@@ -62,7 +62,7 @@ var CORATEST = (function(coraTest) {
 	return coraTest;
 }(CORATEST || {}));
 
-QUnit.module.only("presentation/pCollectionVarTest.js", {
+QUnit.module("presentation/pCollectionVarTest.js", {
 	beforeEach: function() {
 		this.fixture = document.getElementById("qunit-fixture");
 		this.metadataProvider = new MetadataProviderStub();
